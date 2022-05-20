38dd4be35ca4356f708ad886f3249367e6f5a687
rakutentech-ci-publisher <59241113+rakutentech-ci-publisher@users.noreply.github.com>
2022-05-20 16:08:07 +0900

    update Package.swift for version 9.4.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RAnalytics",
-            url: "https://github.com/rakutentech/ios-analytics-framework/releases/download/9.4.0/RAnalyticsRelease-v9.4.0.zip",
-            checksum: "ff95261183405110f3878462b494f31b770b1c8856276a8fc3f7ee4ee446551d"
+            url: "https://github.com/rakutentech/ios-analytics-framework/releases/download/9.4.1/RAnalyticsRelease-v9.4.1.zip",
+            checksum: "3cb209fd4f5b2bd37575dbd2676a4069e8f62aba0b702d20e4a7ae25e10c702c"
         )
     ]
 )
