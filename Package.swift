32906bbdfc61a3729ebd6d9fd7efa6020f48bf26
rakutentech-ci-publisher <59241113+rakutentech-ci-publisher@users.noreply.github.com>
2023-06-05 10:11:56 +0200

    update Package.swift for version 10.0.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RAnalytics",
-            url: "https://github.com/rakutentech/ios-analytics-framework/releases/download/9.8.1/RAnalyticsRelease-v9.8.1.zip",
-            checksum: "b30f6f77deb4282f53ba2f9fe23962b6ca4d517c6cc696a773dab165a37f5758"
+            url: "https://github.com/rakutentech/ios-analytics-framework/releases/download/10.0.0/RAnalyticsRelease-v10.0.0.zip",
+            checksum: "546bf73111d10927d31b5a3f325da506fd0e5c23b1b119df5c90d3e1c1deeecb"
         )
     ]
 )
